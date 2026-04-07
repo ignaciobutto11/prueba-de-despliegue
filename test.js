082af8beafbef739735017de36ca33767ccd25e7
ignacio Butto <ignaciobtto@gmail.com>
2026-04-07 18:54:12 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,26 @@
+function suma(a, b) {
+  return a + b;
+}
+
+function resta(a, b) {
+  return a - b;
+}
+
+// TEST 1
+if (suma(2, 2) !== 5) {
+  throw new Error(" Error en suma");
+}
+
+
+if (resta(5, 3) !== 2) {
+  throw new Error(" Error en resta");
+}
+
+
+if (suma(-1, -1) !== -2) {
+  throw new Error(" Error con negativos");
+}
+
+console.log("Todos los tests pasaron");
+
+
